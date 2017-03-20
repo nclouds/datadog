25805b666e8c0ce444de1948913ca2f03f68278b
Shan <shanmugakarna89@gmail.com>
2017-03-20 23:32:32 +0530

    Initial Commit

---
+++ terraform/monitors.tf
@@ -0,0 +1,26 @@
+# Create a new Datadog monitor
+resource "datadog_monitor" "foo" {
+  name               = "Name for monitor foo"
+  type               = "metric alert"
+  message            = "Monitor triggered. Notify: @hipchat-channel"
+  escalation_message = "Escalation message @pagerduty"
+
+  query = "avg(last_1m):abs(avg:system.mem.used{*}/avg:system.mem.total{*}) > 0.9"
+
+  thresholds {
+    ok       = 0
+    warning  = 0.8
+    critical = 0.9
+  }
+
+  notify_no_data    = true
+  renotify_interval = 60
+
+  notify_audit = false
+  timeout_h    = 60
+  include_tags = true
+
+  silenced {
+    "*" = 0
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+datadog_api_key = "59fe3f4f555ce025a2793058eae90de7"
+datadog_app_key = "fa73c0f3e0966301f02dab54c232f3fb9ee96d00"
